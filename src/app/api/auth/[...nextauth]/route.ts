9f5d0fc644808b91ef2ed3fd88cf8699a4e0f78e
harendra <jeaturajabundela@gmail.com>
2025-07-04 20:33:30 +0530

    next13 bug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+ const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
