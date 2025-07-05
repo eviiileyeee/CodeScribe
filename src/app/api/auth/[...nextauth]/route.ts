c509a2ea2aa76cb07f6bc97e37abd8323735444f
harendra <jeaturajabundela@gmail.com>
2025-07-05 17:19:08 +0530

    route protection using nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
- const authOptions = {
+export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
---
+++ src/lib/auth.ts
@@ -0,0 +1,7 @@
+// lib/auth.ts
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export async function auth() {
+  return await getServerSession(authOptions);
+}
