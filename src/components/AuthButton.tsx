afb86b7f3cae494df73d69b2fe7653a3f1d8ae8f
harendra <jeaturajabundela@gmail.com>
2025-07-21 12:59:56 +0530

    random

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -13,7 +13,6 @@ export default function AuthButton() {
     return (
       <div className="flex items-center gap-2">
         <Skeleton className="h-10 w-10 rounded-full" />
-        <Skeleton className="h-4 w-24 rounded" />
       </div>
     );
   }
